6e450bc3f718464812a5f75ba538395f1089c4a8
koba <38810208+kkkobbb@users.noreply.github.com>
2020-07-08 12:29:33 +0900

    コード整理

--- transi.sh
+++ transi.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 # 英語->日本語 翻訳 (インタラクティブ)
-# 改行を取り除いて翻訳にかける
-# 改行のみの行が入力されると翻訳を実行する
-# 終了はCtrl+c
+#
 # Translate-shell使用
 #   sudo apt install translate-shell
+#
+# Usage:
+#   基本
+#     実行すると"# input text:"とでるので英文(空行なしの複数行)を入力し、
+#     改行のみの行を入力すると日本語に翻訳した結果を表示する
+#     翻訳前に入力した英文の改行は削除し、複数の空白は1つにする
+#   簡易実行
+#     行の最後に";;"があるとそれまでの入力を翻訳して結果を表示する
+#   終了
+#     Ctrl+c or Ctrl+d
 
 PROMPT_INPUT="\n\e[36;1m# input text:\e[0m\n"
 PROMPT_SRC="\n\e[32m# src text:\e[0m\n"
 PROMPT_TRANS="\e[35m# translate:\e[0m\n"
 
-MARK_START_TRANS=";;$"
+REGEX_START_TRANS=";;$"
 
+
+# 引数の日本語を英語に翻訳して標準出力に出力する
+translate_en_ja() {
+	text="$1"
+	trans -no-warn -b en:ja "$text"
+}
 
 printf "$PROMPT_INPUT"
 text=""
 while read LINE || [ -n "$LINE" ]; do
 	continue_text=true
-	add_detail=true
+	detail_flag=true
 
-	# MARK_START_TRANSを含む場合、翻訳を実行する
-	l=$(echo $LINE | sed -n "s/$MARK_START_TRANS//p")
+	# REGEX_START_TRANSにマッチする場合、翻訳を実行する
+	l=$(echo $LINE | sed -n "s/$REGEX_START_TRANS//p")
 	if [ -n "$l" ]; then
 		LINE=$l
 		continue_text=false
-		add_detail=false
+		detail_flag=false
 	fi
 
 	if [ -n "$LINE" ]; then
@@ -45,11 +59,11 @@ while read LINE || [ -n "$LINE" ]; do
 		continue
 	fi
 
-	$add_detail && printf "$PROMPT_SRC"
-	$add_detail && echo $text | fmt
+	$detail_flag && printf "$PROMPT_SRC"
+	$detail_flag && echo $text | fmt
 
-	$add_detail && printf "$PROMPT_TRANS"
-	trans -no-warn -b en:ja "$text"
+	$detail_flag && printf "$PROMPT_TRANS"
+	translate_en_ja "$text"
 
 	printf "$PROMPT_INPUT"
 	text=""
